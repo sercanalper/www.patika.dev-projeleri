b8af6d4d397ab36934c221a2429bbedafdab12c3
Sercan Alper Şahin <sercanalper92@gmail.com>
2022-11-30 09:51:13 +0300

    Arrayların Harmonik Ortalaması

---
+++ HarmonicArray.java
@@ -0,0 +1,13 @@
+public class HarmonicArray {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += (1 / numbers[i]);
+        }
+
+        System.out.println(sum / numbers.length);
+    }
+}
+
+
